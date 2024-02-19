4f34db29613d8094786e6a42c0c8b89f3ba91e13
Iasmin Angel <143419093+IasminAngel@users.noreply.github.com>
2024-02-19 08:07:19 -0300

    Aula_1602_EstoqueRepeticao.c

--- Aula_1602_EstoqueRepeticao.c
+++ Aula_1602_EstoqueRepeticao.c
@@ -11,46 +11,46 @@ int main()
     int i, y;
     float quantia, somativa, vmd, tr[4], emin, lr[4], emax, ea[4];
 
-    printf("Bem-vindo ao estoque! Para sabermos o andamento, será solicitado informações importantes para prosseguimento. \n");
+    printf("Bem-vindo ao estoque! Para sabermos o andamento, ser solicitado informaÃ§Ãµees importantes para prosseguimento. \n");
 
     for (i = 0; i < 4; i++)
     {
-        printf("Lista dos produtos - %dº produto: \n", i + 1);
+        printf("Lista dos produtos - %dÂº produto: \n", i + 1);
         fflush(stdin);
         gets(produtos);
 
         for (y = 0; y < 3; y++)
         {
-            printf("Quais foram a quantia de cada produto dentro de três meses ? Responda em sequência: \n", i + 1);
+            printf("Quais foram a quantia de cada produto dentro de trÃªs meses ? Responda em sequÃªncia: \n", i + 1);
             scanf("%f", &quantia);
             somativa += quantia;
             vmd = (somativa / 3) / 25;
         }
 
-        printf("Ótimo! Revelando os resultados das informacoes aplicadas:\n");
+        printf("Ã“timo! Revelando os resultados das informacoes aplicadas:\n");
         printf("Produtos escolhidos: %s \n", produtos);
         printf("A venda media diaria: %.2f\n", vmd);
 
         printf("Para completar com o nosso estoque, digite o tempo de reposicao:\n");
-        scanf("%f", &tr);
+        scanf("%f", &tr[4]);
 
         emin = (vmd * tr[4]);
 
-        printf("Resultado do estoque mínimo: %.2f \n", emin);
+        printf("Resultado do estoque mÃ­nimo: %.2f \n", emin);
 
-        printf("Para prosseguirmos, precisamos do lote de reposição:\n");
-        scanf("%f", &lr);
+        printf("Para prosseguirmos, precisamos do lote de reposiÃ§Ã£o:\n");
+        scanf("%f", &lr[4]);
 
         emax = emin + lr[4];
 
-        printf("O estoque máximo: %.2f\n", emax);
+        printf("O estoque mÃ¡ximo: %.2f\n", emax);
 
         printf("Para finalizarmos e sabermos o que deve ou nao deve repor, digite o estoque atual:\n");
-        scanf("%f", &ea);
+        scanf("%f", &ea[4]);
 
         if (ea[4] >= emin)
         {
-            printf("Nao comprar!\n");
+            printf("NÃ£o comprar!\n");
         }
         else
         {
